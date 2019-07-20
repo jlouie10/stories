49358e006da805c941644584a1d5c3ae75170e73
justin <jlouie10@gmail.com>
2019-07-20 08:25:31 -0500

    Add GET /api/comments route

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,17 @@ const db = require('../models');
 
 // Routes
 module.exports = function (app) {
+    // GET route for listing all comments
+    app.get('/api/comments', function (req, res) {
+        db.Comment.find({})
+            .then(function (dbComment) {
+                res.status(200).json(dbComment);
+            })
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    });
+
     // POST route for creating a comment and updating a story
     app.post('/api/comments', function (req, res) {
         db.Comment.create(req.body)
