27b808c163e333c1ff2e0db49ef1cc87109b4990
justin <jlouie10@gmail.com>
2019-07-20 08:14:17 -0500

    Add POST /api/stories/:_id route

--- routes/stories.js
+++ routes/stories.js
@@ -28,4 +28,16 @@ module.exports = function (app) {
                 res.status(500).json(err);
             });
     });
+
+    // POST route for updating a story
+    app.post('/api/stories/:_id', function (req, res) {
+        db.Story.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true })
+            .populate('comments')
+            .then(function (dbStory) {
+                res.status(200).json(dbStory);
+            })
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    });
 };
