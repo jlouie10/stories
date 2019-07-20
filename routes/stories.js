2e99d46092ad6acaf6253e5fadd061ec158b7bf2
justin <jlouie10@gmail.com>
2019-07-20 08:11:15 -0500

    Add GET /api/stories/:_id route

--- routes/stories.js
+++ routes/stories.js
@@ -16,4 +16,16 @@ module.exports = function (app) {
                 res.status(500).json(err);
             });
     });
+
+    // GET route for retrieving a story by id
+    app.get('/api/stories/:_id', function (req, res) {
+        db.Story.find({ _id: req.params._id })
+            .populate('comments')
+            .then(function (dbStory) {
+                res.status(200).json(dbStory);
+            })
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    });
 };
