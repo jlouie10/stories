e5ba1119e0de1b94274db384227d87dfd1572ade
justin <jlouie10@gmail.com>
2019-07-21 21:05:39 -0500

    Add GET /api/publishers/:_id route

--- routes/publishers.js
+++ routes/publishers.js
@@ -18,6 +18,18 @@ module.exports = function (app) {
             });
     });
 
+    // GET route for retrieving a publisher by id
+    app.get('/api/publishers/:_id', function (req, res) {
+        db.Publisher.find({ _id: req.params._id })
+            .populate({ path: 'stories', options: { sort: { _id: -1 } } })
+            .then(function (dbPublisher) {
+                res.status(200).json(dbPublisher);
+            })
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    });
+
     // POST route for creating a publisher
     app.post('/api/publishers', function (req, res) {
         db.Publisher.create(req.body)
