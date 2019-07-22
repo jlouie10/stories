680a23934dfad02486455091b4012d9827a05f72
justin <jlouie10@gmail.com>
2019-07-21 21:05:55 -0500

    Add POST /api/publishers/:_id route

--- routes/publishers.js
+++ routes/publishers.js
@@ -40,4 +40,17 @@ module.exports = function (app) {
                 res.status(500).json(err);
             });
     });
+
+    // POST route for updating a publisher
+    app.post('/api/publishers/:_id', function (req, res) {
+        db.Publisher.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true })
+            .populate({ path: 'stories', options: { sort: { _id: -1 } } })
+            .sort({ _id: -1 })
+            .then(function (dbPublisher) {
+                res.status(200).json(dbPublisher);
+            })
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    });
 };
