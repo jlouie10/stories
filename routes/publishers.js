788c2bc853c05c4e9146fdfae90b6af1e6cbd61b
justin <jlouie10@gmail.com>
2019-07-21 21:05:22 -0500

    Add GET /api/publishers route

--- routes/publishers.js
+++ routes/publishers.js
@@ -5,6 +5,19 @@ const db = require('../models');
 
 // Routes
 module.exports = function (app) {
+    // GET route for listing all publishers
+    app.get('/api/publishers', function (req, res) {
+        db.Publisher.find({})
+            .populate({ path: 'stories', options: { sort: { _id: -1 } } })
+            .sort({ _id: -1 })
+            .then(function (dbPublisher) {
+                res.status(200).json(dbPublisher);
+            })
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    });
+
     // POST route for creating a publisher
     app.post('/api/publishers', function (req, res) {
         db.Publisher.create(req.body)
